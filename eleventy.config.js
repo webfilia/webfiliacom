641f43092c37b66a659aa3b57829c188f90cbf68
edusl <eduardosainz@gmail.com>
2026-02-11 19:42:05 +0100

    Actualizamos

--- eleventy.config.js
+++ eleventy.config.js
@@ -25,7 +25,9 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
-		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
+
+  		.addPassthroughCopy("CNAME");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
